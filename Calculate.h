fd1dff08b18efa0910c6918de6e4872b2a45f6fe
Tanahara Koa <k024g0007@g.neec.ac.jp>
2024-11-19 12:59:05 +0900

    三平方関数の修正

--- Calculate.h
+++ Calculate.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Structures.h"
+
+
 // 階乗関数
 int Factorial(int n);
 
@@ -13,6 +16,6 @@ int Binomial(int n, int k);
 float Bernstein(int n, int i, float t);
 
 // 2点の距離の関数
-//float CalculateDistance(const Vector2& a, const Vector2& b);
+float CalculateDistance(const Vector2& a, const Vector2& b);
 
 
